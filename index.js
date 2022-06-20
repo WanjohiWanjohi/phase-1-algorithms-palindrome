401b58950d89adfaf4f4c42b9b90a6e66356bdbe
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-20 22:29:21 +0300

    commit tree 1b07a3eacf0b52c9ba140e8a0ddc39611d30dd9f

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const { consoleLog } = require("mocha/lib/reporters/base");
+
 function isPalindrome(word) {
+  let len = word.length;
+
+  console.log(len);
   // Write your algorithm here
+  for (let i = 0; i < len / 2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
